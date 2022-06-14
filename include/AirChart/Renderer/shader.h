82731462543718a686e657eb3cc9458f58ec02e6
Kereem <akerem1434@gmail.com>
2022-06-14 17:07:23 +0300

    added shader class

---
+++ include/AirChart/Renderer/shader.h
@@ -0,0 +1,31 @@
+#ifndef AC_SHADER_H
+#define AC_SHADER_H
+
+#include <glm/glm.hpp>
+
+namespace AirChart
+{
+    class Shader
+    {
+        public:
+            Shader(const char* vertexPath, const char* fragmentPath);
+            ~Shader();
+
+            void use();
+
+            void setFLoat(const char* name, const float value);
+            void setInt(const char* name, const int value);
+            void setVec2(const char* name, const glm::vec2& value);
+            void setVec3(const char* name, const glm::vec3& value);
+            void setVec4(const char* name, const glm::vec4& value);
+            void setMat4(const char* name, const glm::mat4* ptrValue);
+            void setMat3(const char* name, const glm::mat3* ptrValue);
+
+        protected:
+            unsigned int program;
+            int getUniformLocation(const char* varName);
+            int status(unsigned int T, unsigned int type);
+    };
+};
+
+#endif
