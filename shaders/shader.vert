7bcbef2344b23ebb984af61f2aa29af24fb90d6f
Kereem <akerem1434@gmail.com>
2022-06-14 17:08:44 +0300

    test3

---
+++ shaders/shader.vert
@@ -0,0 +1,8 @@
+#version 460 core
+
+layout (location = 0) in vec2 inPosition;
+
+void main()
+{   
+    gl_Position = vec4(inPosition, 0.0f, 1.0f);
+}
--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,10 @@
-#include<AirChart/window.h>
-#include<AirChart/ogl.h>
+#include<glad/glad.h>
 
+#include<AirChart/Core/window.h>
+#include<AirChart/Renderer/ogl.h>
+#include<AirChart/Renderer/shader.h>
+#include<AirChart/Renderer/vertexarray.h>
+#include<AirChart/Renderer/vertexbuffer.h>
 using namespace AirChart;
 
 int main() {
@@ -10,11 +14,32 @@ int main() {
 
     GL::init();
 
+    Shader shader("./../shaders/shader.vert", "./../shaders/shader.frag");
+
+    float vertices[] = {
+        -0.5f, -0.5f,
+        0.5f, -0.5f,
+        0.0f, 0.5f,
+    };
+
+
+    VertexArray va;
+    va.bind();
+    VertexBuffer vb;
+    vb.bind();
+    vb.bufferData(vertices, sizeof(vertices));
+    va.vertexAttrib(0, 2, sizeof(float) * 2, 0);
+
     GL::setClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
     while(!window.isClosed()) {
         window.pollEvents();
         GL::clear();
+
+        shader.use();
+        va.bind();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         window.update();
     }
     Window::terminate();
