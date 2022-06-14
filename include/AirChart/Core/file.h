2f231a49b5c6212d37fbdbfa8514ae541957b4b4
Kereem <akerem1434@gmail.com>
2022-06-14 17:08:11 +0300

    adde file class

---
+++ include/AirChart/Core/file.h
@@ -0,0 +1,15 @@
+#ifndef AC_FILE_H
+#define AC_FILE_H
+
+#include<string>
+
+namespace AirChart
+{
+    namespace File
+    {
+        std::string openFile(const char* fileName);
+
+    };
+};
+
+#endif
