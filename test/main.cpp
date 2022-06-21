98933775b6693910fc642906a2dd1ed563606f3e
Kereem <akerem1434@gmail.com>
2022-06-21 17:46:33 +0300

    test6

--- test/main.cpp
+++ test/main.cpp
@@ -17,6 +17,15 @@
 
 #include<cmath>
 
+#include <nlohmann/json.hpp>
+#include <curl/curl.h>
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
 using namespace AirChart;
 
 glm::vec2 scale(1.0f);
@@ -49,26 +58,61 @@ int main() {
 
     Shader shader("./shaders/candlebar.vert", "./shaders/shader.frag");
 
-    GL::setClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    GL::setClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
     std::vector<float> open;
     std::vector<float> close;
     std::vector<float> low;
     std::vector<float> high;
 
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<> dis(5.0f, 10.0f);
-    std::uniform_real_distribution<> h(7.2f, 10.0f);
-    std::uniform_real_distribution<> l(5.0f, 8.0f);
+    CURL* curl;
+    CURLcode res;
+    std::string readBuffer;
+    curl_global_init(CURL_GLOBAL_ALL);
 
-    for(int i = 0; i < 40; i++)
-    {
-        open.push_back(dis(gen));
-        close.push_back(dis(gen));
-        low.push_back(l(gen));
-        high.push_back(h(gen));
+    curl = curl_easy_init();
+
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://query1.finance.yahoo.com/v8/finance/chart/BTC-USD?range=1mo&region=US&interval=1d&lang=en");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        res = curl_easy_perform(curl);
+
+        if (res!=CURLE_OK)
+        {
+            fprintf(stderr, "ERROR: curl_easy_perform() returned %s\n", curl_easy_strerror(res));
+
+        }
+
+        
+        curl_easy_cleanup(curl);
     }
+    
+    curl_global_cleanup();
+
+    auto data = nlohmann::json::parse(readBuffer);
+
+    for (int i = 0; i < 30; i++)
+    {
+        high.push_back(data["chart"]["result"][0]["indicators"]["quote"][0]["high"][i]);
+        close.push_back(data["chart"]["result"][0]["indicators"]["quote"][0]["close"][i]);
+        open.push_back(data["chart"]["result"][0]["indicators"]["quote"][0]["open"][i]);
+        low.push_back(data["chart"]["result"][0]["indicators"]["quote"][0]["low"][i]);
+    }
+
+    //std::random_device rd;
+    //std::mt19937 gen(rd());
+    //std::uniform_real_distribution<> dis(5.0f, 10.0f);
+    //std::uniform_real_distribution<> h(7.2f, 10.0f);
+    //std::uniform_real_distribution<> l(5.0f, 8.0f);
+//
+    //for(int i = 0; i < 40; i++)
+    //{
+    //    open.push_back(dis(gen));
+    //    close.push_back(dis(gen));
+    //    low.push_back(l(gen));
+    //    high.push_back(h(gen));
+    //}
 
     glm::vec2 dpos(0.0f);
 
@@ -76,8 +120,11 @@ int main() {
     float deltaTime = 0.0f;
     float lastFrame = 0.0f;
 
+    float width = 0.05f;
+    float spacing = 0.03f;
+    float height = 0.00005f;
 
-    AirChart::Graph::CandleGraph candle(open, close, low, high, 0.05f, 0.03f, 0.1f);
+    AirChart::Graph::CandleGraph candle(open, close, low, high, width, spacing, height);
 
     while(!window.isClosed()) {
         window.pollEvents();
@@ -106,16 +153,16 @@ int main() {
         // check plus key
         if(glfwGetKey(window.getWindow(), GLFW_KEY_KP_ADD) == GLFW_PRESS)
         {
-            *candle.spacing += 0.01f*deltaTime;
-            *candle.width += 0.02f*deltaTime;
-            *candle.height += 0.02f*deltaTime;
+            *candle.spacing += spacing*deltaTime;
+            *candle.width += width*deltaTime;
+            *candle.height += height*deltaTime;
         }
         // check minus key
         if(glfwGetKey(window.getWindow(), GLFW_KEY_KP_SUBTRACT) == GLFW_PRESS)
         {
-            *candle.spacing -= 0.01f*deltaTime;
-            *candle.width -= 0.02f*deltaTime;
-            *candle.height -= 0.02f*deltaTime;
+            *candle.spacing -= spacing*deltaTime;
+            *candle.width -= width*deltaTime;
+            *candle.height -= height*deltaTime;
         }
 
         dpos = cpos;
